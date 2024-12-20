c18e9d464e89793be48fbc9e9051186461a18c44
Farid <faridfaracli9@gmail.com>
2024-12-20 22:16:55 +0400

    implement -i option

--- ls.c
+++ ls.c
@@ -7,36 +7,27 @@
 struct {
 	int a;
 	int A;
+	int i;
 } flags;
 
-int
-main(int argc, char **argv)
+/* FIXME: find a better way */
+static int
+should_print(const char *filename)
+{
+	return filename[0] != '.' || flags.a
+	    || flags.A && strcmp(filename, ".")
+	    && strcmp(filename, "..");
+}
+
+static int
+ls(const char *filepath)
 {
 	DIR *dir;
 	struct dirent *entry;
-	char *filepath;
-	int c;
-
-	opterr = 0;
-	while ((c = getopt(argc, argv, "aA")) != -1) {
-		switch (c) {
-		case 'a':
-			flags.a = 1;
-			break;
-		case 'A':
-			flags.A = 1;
-			break;
-		case '?':
-			fputs("usage: ls [-aA] [DIR]\n", stderr);
-			return 1;
-		}
-	}
-
-	filepath = optind < argc ? argv[optind] : ".";
 
 	if ((dir = opendir(filepath)) == NULL) {
 		fputs("error: opendir", stderr);
-		return 1;
+		return -1;
 	}
 
 	for (;;) {
@@ -44,22 +35,59 @@ main(int argc, char **argv)
 		if ((entry = readdir(dir)) == NULL)
 			break;
 
-		/* FIXME: find a bettry way */
-		if (entry->d_name[0] != '.' || flags.a ||
-		    flags.A && strcmp(entry->d_name, ".") &&
-		    strcmp(entry->d_name, ".."))
+		if (!should_print(entry->d_name))
+			continue;
+
+		if (flags.i)
+			printf("%lu %s\n", entry->d_ino, entry->d_name);
+		else
 			puts(entry->d_name);
 	}
 
 	if (errno) {
 		fputs("error: readdir", stderr);
-		return 1;
+		return -1;
 	}
 
 	if (closedir(dir)) {
 		fputs("error: closedir", stderr);
-		return 1;
+		return -1;
 	}
+
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	const char *filepath;
+	int c;
+
+	opterr = 0;
+	while ((c = getopt(argc, argv, "aAi")) != -1) {
+		switch (c) {
+		case 'a':
+			flags.a = 1;
+			break;
+		case 'A':
+			flags.A = 1;
+			break;
+		case 'i':
+			flags.i = 1;
+			break;
+		case '?':
+			fputs("usage: ls [-aA] [DIR]\n", stderr);
+			return 1;
+		}
+	}
+
+	if (optind < argc)
+		filepath = argv[optind];
+	else
+		filepath = ".";
+
+	if (ls(filepath))
+		return 1;
 
 	return 0;
 }
